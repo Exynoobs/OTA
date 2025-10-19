1197600e0bf6789dfae865478a7fdcd4c9fe1262
Josip Kelecic <kelecicj@gmail.com>
2025-10-19 02:29:45 +0200

    script fixes

--- scripts/autoupload.sh
+++ scripts/autoupload.sh
@@ -15,7 +15,7 @@ DATE=$(echo $FILENAME | cut -f3 -d '-')
 RECOVERY_NAME=$(echo $FILENAME | sed 's/UNOFFICIAL/recovery/g' | sed 's/\.zip/.img/g')
 RELEASENAME="$DEVICE-$DATE"
 
-$SCRIPTDIR/otainfo.sh $ZIP $(hub -C $REPODIR browse -u) $RELEASENAME > $REPODIR/$DEVICE.json
+$SCRIPTDIR/otainfo.sh $ZIP $(hub -C $REPODIR browse -u) $RELEASENAME > $REPODIR/../$DEVICE.json
 cp $BUILDDIR/recovery.img $BUILDDIR/$RECOVERY_NAME
 echo "$(sha256sum $ZIP | cut -f1 -d ' ') $FILENAME" > $REPODIR/$FILENAME.sha256
 echo "$(sha256sum $BUILDDIR/$RECOVERY_NAME | cut -f1 -d ' ') $RECOVERY_NAME" > $REPODIR/$RECOVERY_NAME.sha256
@@ -25,7 +25,7 @@ if [ "$DEVICE" = "dm1q" ]; then
     echo "$(sha256sum $BUILDDIR/vbmeta.img | cut -f1 -d ' ') vbmeta.img" > $REPODIR/vbmeta.img.sha256
 fi
 
-git -C $REPODIR add $DEVICE.json
+git -C $REPODIR add ../$DEVICE.json
 git -C $REPODIR commit -m "OTA: $DEVICE: $DATE"
 git -C $REPODIR tag "$RELEASENAME"
 git -C $REPODIR push origin HEAD:staging --tags
